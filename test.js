906c578adc9138fccd0a8b9421228d480d83f287
Rizqi Raditya <radityaparamita@gmail.com>
2026-03-29 08:15:01 +0700

    hapus file berbahaya

--- test.js
+++
@@ -1,6 +0,0 @@
-const https = require('https');
-https.get('https://generativelanguage.googleapis.com/v1beta/models?key=AIzaSyBd6mifRQ8XjSNVMCYmmDbs6wHjHH8ZMmA', res => {
-  let body = '';
-  res.on('data', d => body += d);
-  res.on('end', () => require('fs').writeFileSync('models.json', body));
-});
